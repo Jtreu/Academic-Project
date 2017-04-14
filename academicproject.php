1a6f943ab090ee03e8209fc1b15306bfbf4218ac
nicolaspatrickward <nicolaspatrickward@gmail.com>
2017-04-13 23:14:08 -0500

    cosmetic support for adding books and homework

--- academicproject.php
+++ academicproject.php
@@ -1,51 +1,36 @@
 <!DOCTYPE html>
 <html>
-
-
 <head>
-	  <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
-		<link rel="stylesheet" href="academicproject.css" </link>
-
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+	<link rel="stylesheet" href="academicproject.css" </link>
 	<meta charset="utf-8">
 	<title>Academic Project</title>
-	<!--<link rel="stylesheet" href="academicproject.css"></link>-->
 </head>
-
 <body>
-
 	<div id="bodyheader">
 		<h1>Reading Progress Database</h1>
 	</div>
 	<div id="studentSelect">
-
-<!--Select list is now populated from actor database "sakila"-->
-<form action="mysql.php" method="post" style = "display: inline">
-	<select name="studentselect">
-	<?php
-	$server = "localhost"; // my local WAMP server, IP address is 153.91.173.160
-	$db = "sakila"; // change out db to ours obviously
-	$user = "root";
-	$password = "";// please don't rape my db
-	$dbconn = mysqli_connect($server, $user, $password, $db)
-	    or die('Could not connect: '.mysqli_connect_error());
-	$sql = mysqli_query($dbconn, "SELECT first_name, last_name FROM actor");
-	while ($row = $sql->fetch_assoc()){
-	echo "<option value=\"" . $row['first_name']. " " . $row['last_name']."\">" . $row['first_name'] . " " . $row['last_name'] . "</option>";
-	}
-
-	?>
-	</select>
-	<input type="submit" name = "submit" id = "bigbutton">
-</form>
-	</div>
-
-
-
-	<!-- ########################################################## -->
-
+		<h3>Select a student to add a book</h3>
+		<form action="mysql.php" method="post" style = "display: inline">
+			<select name="studentselect">
+				<?php
+				$server = "localhost"; // my local WAMP server, IP address is 153.91.173.160
+				$db = "academicdb"; // change out db to ours obviously
+				$user = "root";
+				$password = "";// please don't rape my db
+				$dbconn = mysqli_connect($server, $user, $password, $db)
+	    		or die('Could not connect: '.mysqli_connect_error());
+					$sql = mysqli_query($dbconn, "SELECT first_name, last_name FROM students");
+				while ($row = $sql->fetch_assoc()){
+					echo "<option value=\"" . $row['first_name']. " " . $row['last_name']."\">" . $row['first_name'] . " " . $row['last_name'] . "</option>";
+				}
+				?>
+			</select>
+		<input type="submit" name = "submit" id = "bigbutton">
+	</form>
+</div>
 </body>
-
 </html>
 <script>
-
 </script>
--- mysql.php
+++ mysql.php
@@ -2,53 +2,61 @@
 <head>
   <link rel="stylesheet" href="academicproject.css" </link>
   <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
-
 </head>
 <body>
+  <div id = "studentbooks">
   <!--Select statement from database includes all columns for specified username -->
   <?php
 $selectedStudent = $_POST['studentselect'];
 $selectedStudent = preg_split("/[\s,]+/", $selectedStudent);
 echo "<h1>$selectedStudent[0] $selectedStudent[1]</h1>";
-echo "<button id=\"back\" onclick=\"location.href = 'academicproject.php';\" style='display: block'>Select Different Student</button>";
+echo "<button id=\"back\" onclick=\"location.href = 'academicproject.php';\" style='display: inline'>Select Different Student</button>";
+echo "</div>";
 // Connecting, selecting database
 $server = "localhost";
-$db = "sakila";
+$db = "academicdb";
 $user = "root";
 $password = "";
 $dbconn = mysqli_connect($server, $user, $password, $db)
     or die('Could not connect: '.mysqli_connect_error());
-
-
 // Performing SQL query
-$query = "SELECT * FROM actor WHERE first_name = '$selectedStudent[0]' AND last_name = '$selectedStudent[1]'";
+$query = "SELECT * FROM students WHERE first_name = '$selectedStudent[0]' AND last_name = '$selectedStudent[1]'";
 $result = $dbconn->query($query) or die('Query failed: ' . mysqli_error());
-
-// Printing results in HTML
-echo "<table>\n";
-
-while ($line = $result->fetch_assoc()) {
-
-    echo "\t<tr>\n";
-    foreach ($line as $col_value) {
-        echo "\t\t<td>$col_value</td>\n";
-    }
-    echo "\t</tr>\n";
-}
-echo "</table>\n";
-
-// Free resultset
-mysqli_free_result($result);
-
-// Closing connection
-mysqli_close($dbconn);
 ?>
+
 <!--Need to add database insert functionality here-->
-	<div id="addScore" style="display: block">
+	<div id="addBook" style="display: block">
     <form method="post" style = "display: inline">
-		<input type="text" id="scoreInput"></input>
-		<button onclick="addScore()">Add score</button>
+		<input type="text" placeholder = "book name" id="bookInput" size = "30"></input>
+    <input type="text" placeholder = "level" id="reading_lvlInput" size = "5"></input>
+		<button onclick="" >Submit</button> <!--replace with submit button, post method-->
 	</div>
 
+  <div id="addAssessment" style="display: block">
+    <form method="post" style = "display: inline">
+		<input type="text" placeholder = "assessment name" id="assessment_name" size = "30"></input>
+    <input type="text" placeholder = "grade" id="assessment_grade" size = "5"></input>
+		<button onclick="" >Submit</button><!--replace with submit button, post method-->
+	</div>
+  <?php
+  // Printing results in HTML
+  echo "<table>\n";
+
+  while ($line = $result->fetch_assoc()) {
+
+      echo "\t<tr>\n";
+      foreach ($line as $col_value) {
+          echo "\t\t<td>$col_value</td>\n";
+      }
+      echo "\t</tr>\n";
+  }
+  echo "</table>\n";
+
+  // Free resultset
+  mysqli_free_result($result);
+
+  // Closing connection
+  mysqli_close($dbconn);
+  ?>
 </body>
 </html>
